617705123287c5fd0379f0ef790638f37900840d
M. Ahsin Rasheed Bhatti <65343061+ahsinrasheed5@users.noreply.github.com>
2020-09-04 11:16:49 -0800

    update about test case

--- src/About.test.js
+++ src/About.test.js
@@ -3,7 +3,7 @@ import { render } from '@testing-library/react';
 import About from './About';
 
 test('About Test Case ', ()=>{
-    const { getByText } = render(<App />);
+    const { getByText } = render(<About />);
     const linkElement = getByText(/Hello About/i);
     expect(linkElement).toBeInTheDocument();
 })
